79bfb644be95839efda56046a884089fb7818cf2
yuta02gg <yuta02gg@outlook.jp>
2024-03-06 20:46:01 +0900

    20:45

---
+++ Capsule/src/Capsule.java
@@ -0,0 +1,13 @@
+
+public class Capsule {
+	public static void main(String[]args) {
+		Person taro=new Person("山田太郎",20);
+		System.out.println(taro.getName());
+		System.out.println(taro.getAge());
+		taro.setName("花子");
+		taro.setAge(30);
+		System.out.println(taro.getName());
+		System.out.println(taro.getAge());
+	}
+
+}
